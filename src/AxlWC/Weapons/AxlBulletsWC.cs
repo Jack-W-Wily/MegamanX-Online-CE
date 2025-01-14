a4ca67c0a2d80ae7988bc73da2240694b2f83c2d
Gacel <11762096+Windblade-GR01@users.noreply.github.com>
2025-01-13 22:43:34 -0500

    Update AxlBulletsWC.cs

--- src/AxlWC/Weapons/AxlBulletsWC.cs
+++ src/AxlWC/Weapons/AxlBulletsWC.cs
@@ -138,6 +138,7 @@ public class AxlBulletWCProj : Projectile {
 
 		if (type >= 1) {
 			changeSprite("axl_bullet_blue", true);
+			damager.damage = 2;
 			damager.flinch = Global.miniFlinch;
 		}
 
