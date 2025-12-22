04f4cc79d283968bd415215d891e2d215953cc8b
Jack-W-Wily <120156340+Jack-W-Wily@users.noreply.github.com>
2025-12-21 22:52:47 -0300

    a

--- src/Menu/JoinMenuMK2.cs
+++ src/Menu/JoinMenuMK2.cs
@@ -215,7 +215,7 @@ public class JoinMenuP2P : IMainMenu {
 					}, new MainMenu())
 				);
 			return;
-	/*	} else if (Global.checksum != serverdata.gameChecksum) {
+		} else if (Global.checksum != serverdata.gameChecksum) {
 			exit(
 				new ErrorMenu(new string[] {
 					"Client and server have different",
@@ -224,7 +224,7 @@ public class JoinMenuP2P : IMainMenu {
 					"Theirs: " + serverdata.gameChecksum },
 					new MainMenu())
 				);
-			return;*/
+			return;
 		} else if (!string.IsNullOrEmpty(serverdata.customMapChecksum)) {
 			var myLevelChecksum = LevelData.getChecksumFromName(serverdata.level);
 			if (string.IsNullOrEmpty(myLevelChecksum)) {
